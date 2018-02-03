f15c859762e9cf678c6d62484a3a7ca755cc0ce0
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-02-03 12:50:20 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# pixel-art-maker
+# Pixel Art Maker Project
 This is my final project for Udacity's Grow with Google Challenge
+
+## Table of Contents
+
+* [Instructions](#instructions)
+
+## Instructions
+
+
+
+
+
+
---
+++ designs.js
@@ -0,0 +1,40 @@
+$(function(){
+    let pixelTable = $("#pixelCanvas");
+    function makeGrid(r, c) {
+        let newRow = "<tr></tr>";
+        let newCol = "<td></td>";
+        for(let rows = 0; rows < r; rows++){
+            let thisRow = $(newRow).appendTo(pixelTable);
+            for(let cols = 0; cols < c; cols++){
+                $(newCol).appendTo(thisRow);
+            }
+        }
+    }
+
+    function clearBoard(){
+        let allRows = pixelTable.children();
+        allRows.remove();
+    }
+
+    // Select size input
+    let sizeForm = $("#sizePicker");
+    sizeForm.submit(function(event){
+        event.preventDefault();
+        clearBoard();
+        let heightInput = sizeForm.children().first();
+        let widthInput = sizeForm.children().eq(1);
+        let theHeight = heightInput.val();
+        let theWidth = widthInput.val();
+        makeGrid(theHeight, theWidth); // When size is submitted, call makeGrid()
+    });
+
+    // Select color input
+    var colorPicker = $("#colorPicker"); 
+    var td = pixelTable.find("td");
+    $("#pixelCanvas").on("click", "td", function(event){
+        //console.log(event);
+        var backgroundColor = colorPicker.val();
+        $(this).css("background-color", backgroundColor);
+    });
+    
+})
