f0d55c955fa2d662fc75e915007b84c3ebd48a90
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-06-20 22:27:28 -0500

    Modified code not using jQuery for FEND

--- designs.js
+++ designs.js
@@ -1,40 +1,49 @@
-$(function(){
-    let pixelTable = $("#pixelCanvas");
-    function makeGrid(r, c) {
-        let newRow = "<tr></tr>";
-        let newCol = "<td></td>";
-        for(let rows = 0; rows < r; rows++){
-            let thisRow = $(newRow).appendTo(pixelTable);
-            for(let cols = 0; cols < c; cols++){
-                $(newCol).appendTo(thisRow);
-            }
-        }
+// Select color input
+let color;
+function getColor(event){
+  color = colorPicker.value;
+  if(event){
+ 	console.log('Selected color :' + event.target.value);
+    color = event.target.value;
+  }
+}
+const colorPicker = document.getElementById('colorPicker');
+colorPicker.addEventListener("input", getColor);
+getColor();
+
+// Select size input
+const inputHeight = document.getElementById('inputHeight');
+const inputWidth = document.getElementById('inputWidth');
+const submitBtn = document.querySelector('input[type="submit"]');
+
+// When size is submitted by the user, call makeGrid()
+submitBtn.addEventListener('click', function(event){
+  event.preventDefault();
+  let height = inputHeight.value;
+  let width = inputWidth.value;
+  makeGrid(height, width);
+});
+
+const pixelCanvas = document.getElementById('pixelCanvas');
+function respondToTableClick(event){
+  if(event.target.nodeName === 'TD'){
+ 	  event.target.style.backgroundColor = color; 
+  }
+}
+pixelCanvas.addEventListener('click', respondToTableClick);
+
+function clearGrid(){
+  pixelCanvas.innerHTML = '';
+}
+
+function makeGrid(h, w) {
+  clearGrid();
+  for(let rows = 0; rows < h; rows++){
+    let tr = document.createElement('tr');
+    pixelCanvas.appendChild(tr);
+    for(let cols = 0; cols < w; cols++){
+      tr.appendChild(document.createElement('td'));
     }
-
-    function clearBoard(){
-        let allRows = pixelTable.children();
-        allRows.remove();
-    }
-
-    // Select size input
-    let sizeForm = $("#sizePicker");
-    sizeForm.submit(function(event){
-        event.preventDefault();
-        clearBoard();
-        let heightInput = sizeForm.children().first();
-        let widthInput = sizeForm.children().eq(1);
-        let theHeight = heightInput.val();
-        let theWidth = widthInput.val();
-        makeGrid(theHeight, theWidth); // When size is submitted, call makeGrid()
-    });
-
-    // Select color input
-    let colorPicker = $("#colorPicker"); 
-    let td = pixelTable.find("td");
-    $("#pixelCanvas").on("click", "td", function(event){
-        //console.log(event);
-        let backgroundColor = colorPicker.val();
-        $(this).css("background-color", backgroundColor);
-    });
-    
-})
+  }
+}
+makeGrid(10,10); 
